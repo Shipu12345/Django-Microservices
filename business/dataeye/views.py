616925cc06499f85285087c32f94212e8f10e49f
Shipu <shipankar.shipu@gmail.com>
2023-03-11 03:27:42 +0600

    :sparkles: Filter Stores within 2000 km

--- business/dataeye/views.py
+++ business/dataeye/views.py
@@ -14,6 +14,7 @@ from .models import Store
 from business.auth_client.auth import AuthClient
 from .helpers import RequestDataDistance
 
+
 class StoreViewSet(RetrieveModelMixin, GenericViewSet, ListModelMixin):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
@@ -23,20 +24,25 @@ class StoreViewSet(RetrieveModelMixin, GenericViewSet, ListModelMixin):
         if not is_verified:
             msg = "Invalid Authorization header. No credentials provided."
             raise exceptions.AuthenticationFailed(msg)
-    
+
         get_data = self.request.GET
-        if 'lat' not in  get_data:
+        if "lat" not in get_data:
             msg = "No Lat Provided"
             raise exceptions.APIException(msg)
-        lat  = get_data['lat']
+        lat = get_data["lat"]
 
-        if 'long' not in  get_data:
+        if "long" not in get_data:
             msg = "No Long Provided"
             raise exceptions.APIException(msg)
-        long = get_data['long']
-        distance_obj = RequestDataDistance(lat, long)
+        long = get_data["long"]
+        distance_obj = RequestDataDistance(float(lat), float(long))
 
         print(distance_obj)
 
+        available_store_ids = []
+        for store in Store.objects.all():
+            if distance_obj.get_distance_in_km(lat=store.lat, long=store.long) < 2000:
+                available_store_ids.append(store.id)
+
         # return super().get_queryset()
-        return Store.objects.filter()
+        return Store.objects.filter(id__in=available_store_ids)
